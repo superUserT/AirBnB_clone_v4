939ff9fa18f0933987832a083ddf449e69629fd9
superUserT <rantshothabisomail@gmail.com>
2024-01-12 20:08:01 +0200

    do 2 and 3 updates

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['54.173.9.236', '18.209.152.129']
 
 
 def do_deploy(archive_path):
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['54.173.9.236', '18.209.152.129']
 
 
 def do_pack():
